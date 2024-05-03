c118f5ba2aef398f01d9069ff85304ede47f6fee
ashrefch <ashrefchaabeni7@gmail.com>
2024-05-03 17:08:11 +0100

    fetching all listing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,44 @@
+import getListing from "./actions/getListing";
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/ListingCard";
 
 
-export default function Home() {
+export default async function Home() {
+  const listings = await getListing()
+ 
+  if(listings.length===0) return (
+    <ClientOnly>
+      <EmptyState showReset/>
+    </ClientOnly>
+  )
   return (
-   <div className="text-[#FF385C]">
-    Hello Airbnb
-   </div>
+ <ClientOnly>
+  <Container>
+      <div className="
+        pt-24
+        grid
+        grid-cols-1
+        sm:grid-cols-2
+        md:grid-cols-3
+        lg:grid-cols-4
+        xl:grid-cols-5
+        2xl:grid-cols-6
+        gap-8
+
+
+      ">
+        {listings.map((listing:any)=>{
+          return (
+            <ListingCard
+            key={listing.id}
+            data={listing}
+            />
+          )
+        })}
+      </div>
+  </Container>
+ </ClientOnly>
   );
 }
