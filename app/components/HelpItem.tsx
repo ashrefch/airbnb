4b2707690ec172a2f2065b9ba977a2eda3153095
ashrefch <ashrefchaabeni7@gmail.com>
2024-05-19 18:05:32 +0100

    hello world

--- app/components/HelpItem.tsx
+++ app/components/HelpItem.tsx
@@ -35,7 +35,7 @@ const HelpItem:React.FC<HelpItemProps> =({
         mt-8
         cursor-pointer
         ${active ? 'text-white':'text-black'}
-        ${active ? 'bg-rose-500':'bg-white'}
+        ${active ? 'bg-rose-400':'bg-white'}
         `}>
          <div>{title}</div>
         <Icon size={50}/>
